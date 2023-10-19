eb29db13c5d7d766c8078e34ee581221f807a785
lcmpbll <105952041+lcmpbll@users.noreply.github.com>
2023-10-19 06:30:43 +0000

    Use switch statement for input validation.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> // for rand()
 #include <time.h> //for truly random
-#include <string.h> // for string manipulation
 /******************************************************************************
 # Author:           Liam Campbell
 # Lab:              Assignment #3
@@ -13,55 +12,66 @@
 #******************************************************************************/
 int main(void) {
   char userPlay[2], computerPlay[2], winCondition[50];
-  int randomNum;
-
+  int randomNum, winLossTie;
+  
   //Welcome user get userPlay
   printf("Welcome to the Rock Paper Scissors Game!\n");
   printf("Enter r, p, or s for rock, paper, or scissors respectively\n");
   printf("What is your play?\n");
     // to simplify the rest of the program I would change user input to uppercase or lowercase, after their input 
   scanf("%s", userPlay);
-
-  // if userPlay is not r, p, or s skip the rest of the program
-  if (strcmp(userPlay, "r") == 0 || strcmp(userPlay, "R") == 0 || strcmp(userPlay, "s") == 0 || strcmp(userPlay, "S") == 0 || strcmp(userPlay, "p") == 0 || strcmp(userPlay, "P") == 0) {
-   
+  
     // get random number for computer play, using seed based on time
     srand(time(NULL));
     randomNum = rand() % 3;
-  
-    // Assign computerPlay based on randomNum
-    if(randomNum < 1){
-      strcpy(computerPlay, "r");
-    } else if (randomNum > 1) {
-      strcpy(computerPlay, "p");
-    } else {
-      strcpy(computerPlay, "s");
-    }
 
-    // compare values to decide who wins
-    if(strcmp(computerPlay, userPlay) == 0){
-      // If user and computer play the same thing
-      strcpy(winCondition, "It's a tie!");
-      // evaluate win conditions
-    } else if ((strcmp(userPlay, "r") == 0 || strcmp(userPlay, "R") == 0) && (strcmp(computerPlay, "s") == 0)) {
-      strcpy(winCondition, "You win!");
-    } else if ((strcmp(userPlay, "p") == 0 || strcmp(userPlay, "P") == 0) && (strcmp(computerPlay, "r") == 0)) {
-      strcpy(winCondition, "You win!");
-    } else if ((strcmp(userPlay, "s") == 0 || strcmp(userPlay, "S") == 0) && (strcmp(computerPlay, "p") ==0 )) {
-      strcpy(winCondition, "You win!");
-    } else {
-      // if you don't win or tie you lose
-      strcpy(winCondition, "You lose!");
-    }
-    // tell the user about the outcome
-    printf("Computer plays %s\n", computerPlay);
-    printf("%s\n", winCondition);
-    printf("Thanks for playing!\n");
+  // set computer's play
+    
+  if(randomNum < 1){
+    computerPlay[0] = 'r';
+  } else if (randomNum > 1) {
+    computerPlay[0] = 'p';
   } else {
-    // calling the program again would be helpful at this point
-    printf("Invalid play. Please restart the program.\n");
-
+    computerPlay[0] = 's';
   }
+
+  // if userPlay is not r, p, or s skip the rest of the program
+  switch (userPlay[0]) {
+    case 'r':
+    case 'R':
+    case 'S':
+    case 's':
+    case 'p':
+    case 'P':
+      break;
+    default:
+      printf("Invalid play, please try again.");
+      return 0;
+      break;
+  }
+
+  // Tell the user what their opponent plays
+    printf("Computer plays %c\n", computerPlay[0]);
+  // compare values to decide who wins
+    if(computerPlay[0] == userPlay[0]){
+  // If user and computer play the same thing
+      printf("It's a tie!\n");
+  // Determine who wins
+    } else if (userPlay[0] == 'R' || userPlay[0] == 'r' && computerPlay[0] == 's') {
+      printf("You Win\n");
+    } else if (userPlay[0] == 'P' || userPlay[0] == 'p' && computerPlay[0] == 'r') {
+      printf("You win!\n");
+    } else if (userPlay[0] == 's' || userPlay[0] == 'S' && computerPlay[0] == 'p') {
+      printf("You win!\n");
+    } else {
+ // if you don't win or tie you lose
+      printf("You lose!\n");
+    }
+  
+    printf("Thanks for playing!\n");
+
+
+  
   
   return 0;
 }
